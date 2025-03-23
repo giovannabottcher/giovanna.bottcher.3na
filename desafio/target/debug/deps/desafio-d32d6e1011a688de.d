1f0b75ed7d29f1953dedeb39fc1ec0d9b6b89e69
giovannabottcher <giovannabottcher18@gmail.com>
2025-03-23 23:37:02 +0000

    Desafio

---
+++ desafio/target/debug/deps/desafio-d32d6e1011a688de.d
@@ -0,0 +1,7 @@
+/workspaces/giovanna.bottcher.3na/desafio/target/debug/deps/libdesafio-d32d6e1011a688de.rmeta: src/lib.rs
+
+/workspaces/giovanna.bottcher.3na/desafio/target/debug/deps/libdesafio-d32d6e1011a688de.rlib: src/lib.rs
+
+/workspaces/giovanna.bottcher.3na/desafio/target/debug/deps/desafio-d32d6e1011a688de.d: src/lib.rs
+
+src/lib.rs:
---
+++ desafio/target/debug/deps/desafio-ed99b77cd60a33e4.d
@@ -0,0 +1,5 @@
+/workspaces/giovanna.bottcher.3na/desafio/target/debug/deps/desafio-ed99b77cd60a33e4: src/lib.rs
+
+/workspaces/giovanna.bottcher.3na/desafio/target/debug/deps/desafio-ed99b77cd60a33e4.d: src/lib.rs
+
+src/lib.rs:
